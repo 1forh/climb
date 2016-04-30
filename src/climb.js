0974ddf953973f4717dc4eabbac95f560422a846
1forh <1forhlol@gmail.com>
2016-04-30 19:57:10 -0400

    Remove border from button

--- src/climb.js
+++ src/climb.js
@@ -41,6 +41,7 @@ var Climb = function() {
       'background-color': background,
       'border-top-right-radius': '10px',
       'border-bottom-right-radius': '10px',
+      'border': 'none',
       'cursor': 'pointer',
       'outline': 'none'
     };
@@ -52,6 +53,7 @@ var Climb = function() {
       'background-color': background,
       'border-top-left-radius': '10px',
       'border-bottom-left-radius': '10px',
+      'border': 'none',
       'cursor': 'pointer',
       'outline': 'none'
     };
